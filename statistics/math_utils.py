a0aa9d651ae6d68335bfa33a857ce48f9e1da2fb
Aljaž Šmaljcelj <aljaz.smaljcelj7@gmail.com>
2024-05-21 22:46:08 +0200

    sampling along skeleton done

--- statistics/math_utils.py
+++ statistics/math_utils.py
@@ -25,26 +25,33 @@ def distance_between_points(point1, point2):
     return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2 + (point2[2] - point1[2]) ** 2)
 
 
-def rotate_vector(vector, angle, base_vector):
-    R = [
-        [
-            math.cos(angle) + base_vector[0] ** 2 * (1 - math.cos(angle)),
-            base_vector[0] * base_vector[1] * (1 - math.cos(angle)) - base_vector[2] * math.sin(angle),
-            base_vector[0] * base_vector[2] * (1 - math.cos(angle)) + base_vector[1] * math.sin(angle)
-        ],
-        [
-            base_vector[1] * base_vector[0] * (1 - math.cos(angle)) + base_vector[2] * math.sin(angle),
-            math.cos(angle) + base_vector[1] ** 2 * (1 - math.cos(angle)),
-            base_vector[1] * base_vector[2] * (1 - math.cos(angle)) - base_vector[0] * math.sin(angle)
-        ],
-        [
-            base_vector[2] * base_vector[0] * (1 - math.cos(angle)) - base_vector[1] * math.sin(angle),
-            base_vector[2] * base_vector[1] * (1 - math.cos(angle)) + base_vector[0] * math.sin(angle),
-            math.cos(angle) + base_vector[2] ** 2 * (1 - math.cos(angle))
-        ]
-    ]
-    R = np.array(R)
-    return np.dot(R, vector)
+def rotate_vector(vector, angle_degrees, base_vector):
+    # R = [
+    #     [
+    #         math.cos(angle) + base_vector[0] ** 2 * (1 - math.cos(angle)),
+    #         base_vector[0] * base_vector[1] * (1 - math.cos(angle)) - base_vector[2] * math.sin(angle),
+    #         base_vector[0] * base_vector[2] * (1 - math.cos(angle)) + base_vector[1] * math.sin(angle)
+    #     ],
+    #     [
+    #         base_vector[1] * base_vector[0] * (1 - math.cos(angle)) + base_vector[2] * math.sin(angle),
+    #         math.cos(angle) + base_vector[1] ** 2 * (1 - math.cos(angle)),
+    #         base_vector[1] * base_vector[2] * (1 - math.cos(angle)) - base_vector[0] * math.sin(angle)
+    #     ],
+    #     [
+    #         base_vector[2] * base_vector[0] * (1 - math.cos(angle)) - base_vector[1] * math.sin(angle),
+    #         base_vector[2] * base_vector[1] * (1 - math.cos(angle)) + base_vector[0] * math.sin(angle),
+    #         math.cos(angle) + base_vector[2] ** 2 * (1 - math.cos(angle))
+    #     ]
+    # ]
+    # R = np.array(R)
+    # return np.dot(R, vector)
+    theta = np.deg2rad(angle_degrees)
+    cos_theta = np.cos(theta)
+    sin_theta = np.sin(theta)
+    term1 = vector * cos_theta
+    term2 = np.cross(base_vector, vector) * sin_theta
+    term3 = base_vector * np.dot(base_vector, vector) * (1 - cos_theta)
+    return term1 + term2 + term3
 
 
 def rotate_vector_test(v1, v2, v3, current_vector, angle):
@@ -60,11 +67,31 @@ def rotate_vector_test(v1, v2, v3, current_vector, angle):
 def rotate(p, origin=(0, 0), degrees=0):
     angle = np.deg2rad(degrees)
     R = np.array([[np.cos(angle), -np.sin(angle)],
-                  [np.sin(angle),  np.cos(angle)]])
+                  [np.sin(angle), np.cos(angle)]])
     o = np.atleast_2d(origin)
     p = np.atleast_2d(p)
-    return np.squeeze((R @ (p.T-o.T) + o.T).T)
+    return np.squeeze((R @ (p.T - o.T) + o.T).T)
 
 
 def from_degrees_to_radians(degress):
     return math.pi * degress / 180.0
+
+
+def calculate_angle(vector1, vector2):
+    return np.arccos(np.dot(vector1, vector2))
+
+
+def equation_plane(point1, point2, point3):
+    a1 = point2[0] - point1[0]
+    b1 = point2[1] - point1[1]
+    c1 = point2[2] - point1[2]
+    a2 = point3[0] - point1[0]
+    b2 = point3[1] - point1[1]
+    c2 = point3[2] - point1[2]
+    a = b1 * c2 - b2 * c1
+    b = a2 * c1 - a1 * c2
+    c = a1 * b2 - b1 * a2
+    d = (-a * point1[0] - b * point1[1] - c * point1[2])
+    print('params:', a, b, c, d)
+    print('normal', a, b, c)
+
--- statistics/sampling.py
+++ statistics/sampling.py
@@ -4,24 +4,24 @@ import numpy as np
 
 from bezier import perform_arc_length_parametrization_bezier_curve
 from utils import read_file_collect_points, read_nii_file, plot_sampling_with_shape
-from math_utils import magnitude, rotate_vector, distance_between_points, from_degrees_to_radians
+from math_utils import magnitude, rotate_vector, distance_between_points, from_degrees_to_radians, calculate_angle, equation_plane
 
 
 # todo: neki cudno sampla tudi izven BB-ja????????
-def sample_rays(origin, direction_vector, shape, base_x, parametrized_points, skeleton):
+def sample_rays(origin, direction_vector, shape, base_x, base_y, parametrized_points, skeleton):
     original_direction_vector = direction_vector
     distances = []
     sampled_points = {}
     base_x = base_x / magnitude(base_x)
+    base_y = base_y / magnitude(base_y)
     counter = 0
-    # while not (direction_vector == original_direction_vector).all() or counter < 6:
-    while counter < 6:
+    while not np.allclose(direction_vector, original_direction_vector) or counter == 0:
         distance, sampled_points_a = iterate_ray(origin, direction_vector, shape)
         distances.append(distance)
         sampled_points[counter] = sampled_points_a
-        direction_vector = get_new_direction_vector(direction_vector, original_direction_vector)
+        direction_vector = get_new_direction_vector(original_direction_vector, base_x, counter)
         counter += 1
-    plot_sampling_with_shape(shape, sampled_points, skeleton, parametrized_points)
+    # plot_sampling_with_shape(shape, sampled_points, skeleton, parametrized_points)
     return distances
 
 
@@ -34,7 +34,7 @@ def iterate_ray(origin, direction_vector, shape):
         x_coord, y_coord, z_coord = int(new_point[0]), int(new_point[1]), int(new_point[2])
         previous_voxel = current_voxel
         current_voxel = [x_coord, y_coord, z_coord]
-        in_boundary = shape[x_coord][y_coord][z_coord] > 0
+        in_boundary = x_coord < shape.shape[0] and y_coord < shape.shape[1] and z_coord < shape.shape[2] and shape[x_coord][y_coord][z_coord] > 0
         # todo: improved collision detection and distance measurement
         if not in_boundary:
             # distance += magnitude(direction_vector)
@@ -42,20 +42,20 @@ def iterate_ray(origin, direction_vector, shape):
             distance = distance_between_points(origin, boundary_voxel)
             break
         sampled_points_a.append(new_point)
-        # else:
-        #     pass
         current_point = new_point
     return distance, sampled_points_a
 
 
-def get_new_direction_vector(previous_vector, base_x):
-    return rotate_vector(previous_vector, from_degrees_to_radians(45), base_x)
+def get_new_direction_vector(previous_vector, base_x, counter):
+    return rotate_vector(previous_vector, counter + 1, base_x)
 
 
 if __name__ == '__main__':
     skeletons_folder = '../skeletons/'
     num_of_points = 10
     for filename in os.listdir(skeletons_folder):
+        # if filename != 'fib1-3-3-0_41.csv':
+        #     continue
         distances = {}
         points = read_file_collect_points(filename, skeletons_folder)
         object_points = read_nii_file('../extracted_data/', filename.replace('.csv', '.nii'))
@@ -71,11 +71,12 @@ if __name__ == '__main__':
                 # normala
                 a = next_point - current_point
                 b = previous_point - current_point
+                a = a / magnitude(a)
+                b = b / magnitude(b)
                 normal = np.cross(a, b)
-                mag = magnitude(normal)
-                normal = normal / mag
-                distances[i] = sample_rays(current_point, normal, object_points, a, arc, points)
-                break
-        break
+                normal = normal / magnitude(normal)
+                base_y = np.cross(a, normal)
+                base_y = base_y / magnitude(base_y)
+                # equation_plane(current_point, previous_point, next_point)
+                distances[i] = sample_rays(current_point, normal, object_points, a, base_y, arc, points)
         print(distances)
-        break
